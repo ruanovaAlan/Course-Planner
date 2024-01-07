98bf590cbd6b95e527ffe276c253fa1352fd54b0
Alan Ruanova <ruanovaalan96@gmail.com>
2024-01-07 15:30:52 -0600

    Add FullCalendar components and Calendar.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Header from './components/Header'
+import Calendar from './components/Calendar'
 
 function App() {
 
   return (
     <>
       <Header />
+      <Calendar />
     </>
   )
 }
