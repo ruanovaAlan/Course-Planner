b31d2c42dc2155ccb8b66dd43d52bdefbecbab83
Alan Ruanova <ruanovaalan96@gmail.com>
2024-01-07 16:18:42 -0600

    Add @fullcalendar/daygrid plugin and implement calendar component

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -1,5 +1,24 @@
+import FullCalendar from '@fullcalendar/react'
+import dayGridPlugin from '@fullcalendar/daygrid'
+
+
 export default function Calendar() {
     return (
-        <h1>Calendar</h1>
+        <>
+            <div className='size-4/5 m-auto' >
+                <FullCalendar
+                    plugins={[dayGridPlugin]}
+                    initialView="dayGridWeek"
+                    events={[
+                        { title: 'event 1', date: '2024-01-07' },
+                        { title: 'event 2', date: '2024-01-08' }
+                    ]}
+                    headerToolbar={false}
+                    aspectRatio={1.5}
+
+
+                />
+            </div>
+        </>
     )
 }
