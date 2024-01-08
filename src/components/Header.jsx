d2712fc8aa0450364715e114d396af985b24454d
Alan Ruanova <ruanovaalan96@gmail.com>
2024-01-08 15:45:26 -0600

    Update header text to Spanish

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ export default function Header() {
     return (
         <div className="flex flex-col items-center mt-8 mb-8">
             <img className="w-44 h-auto mb-4" src={logo} alt="App Logo" />
-            <h1 className="text-center text-xl md:text-4xl">Course Planner</h1>
+            <h1 className="text-center text-xl md:text-4xl">Planificador Semanal</h1>
         </div>
     );
 }
