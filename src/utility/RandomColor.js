6b6f7fd509385eb3c1dc96dcc9d5a78e4a02ac93
Alan Ruanova <ruanovaalan96@gmail.com>
2024-01-10 18:33:00 -0600

    Update available colors in RandomColor.js

--- src/utility/RandomColor.js
+++ src/utility/RandomColor.js
@@ -1,11 +1,11 @@
 
-const colors = ['blue', 'orange', 'green', 'red', 'purple', 'yellow', 'pink', 'gray',
-    'cyan', 'magenta', 'lime', 'teal', 'lavender', 'maroon', 'navy', 'olive', 'silver',
+const colors = ['blue', 'orange', 'green', 'red', 'purple', 'pink',
+    'cyan', 'magenta', 'teal', 'lavender', 'navy', 'olive',
     'darkgreen', 'darkblue', 'darkred', 'darkgray', 'darkorange', 'darkcyan', 'darkmagenta',
     'darkviolet', 'darkpink', 'darkbrown', 'darkyellow', 'darkteal', 'darkolive', 'darklime',
-    'darknavy', 'darkmaroon', 'lightgreen', 'lightblue', 'lightred', 'lightgray',
-    'lightorange', 'lightcyan', 'lightmagenta', 'lightviolet', 'lightpink', 'lightbrown',
-    'lightyellow', 'lightteal', 'lightolive', 'lightlime', 'lightnavy', 'lightmaroon']
+    'darknavy', 'lightgreen', 'lightblue', 'lightred', 'lightgray',
+    'lightorange', 'lightcyan', 'lightmagenta', 'lightviolet', 'lightpink',
+    'lightteal', 'lightolive', 'lightnavy']
 
 export function randomColor() {
     return colors[Math.floor(Math.random() * colors.length)]
